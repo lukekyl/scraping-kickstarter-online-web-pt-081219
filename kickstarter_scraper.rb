4a5dfc95166dde7359ebecdc0109ac6732ab6b12
Kyle Luke <th.oldschool@gmail.com>
2019-09-23 22:23:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,7 +20,6 @@ def create_project_hash
   kickstarter.css("liprojects.grid_4").each { |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] ={
-
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("span.location-name").text,
