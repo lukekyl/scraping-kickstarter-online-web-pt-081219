60d48a7228ebf673f05028f1df03be46c9c03dbc
Kyle Luke <th.oldschool@gmail.com>
2019-09-23 22:25:35 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,3 +1,11 @@
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
+# percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
+
 # require libraries/modules here
 require "pry"
 require "nokogiri"
@@ -10,14 +18,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
-  # projects: kickstarter.css("li.project.grid_4")
-  # title: project.css("h2.bbcard_name strong a").text
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text
-  # location: project.css("span.location-name").text
-  # percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
-  kickstarter.css("liprojects.grid_4").each { |project|
+  kickstarter.css("li.projects.grid_4").each { |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
